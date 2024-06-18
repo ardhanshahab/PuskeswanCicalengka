175f90a5d3ea861eed2ad881546f68ea97ad666c
Muhamad Ardhan Hadaina <65588203+ardhanshahab@users.noreply.github.com>
2024-06-18 17:42:49 +0700

    fixing minor v2

--- resources/views/pembayaran/index.blade.php
+++ resources/views/pembayaran/index.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12 d-flex justify-content-center my-4">
-            <h4>Pembayaran</h4>
+<div class="container-fluid d-flex justify-content-center align-items-center vh-100">
+    <div>
+        <div class="row text-center">
+            <div class="col-md-12 my-4">
+                <h4>Pembayaran</h4>
+                <h4>BCA : 2832114139 Atas Nama Lola Meilani</h4>
+            </div>
+            <div class="col-md-12 my-4">
+                <p>atau segera ke bagian administrasi untuk pembayarannya</p>
+            </div>
+            <div class="col-md-12 my-4">
+                <a href="javascript:history.back()" class="btn btn-primary">Back</a>
+            </div>
         </div>
     </div>
 </div>
